55bc7b35b4821784786c519b24694f5442757249
daniel527 <62884278+daniel527@users.noreply.github.com>
2021-12-29 09:22:00 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import FullCalendarApp from './components/FullCalendarApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+        <FullCalendarApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FullCalendarApp.js
@@ -0,0 +1,47 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+const events = [
+  {
+    id: 1,
+    title: 'event 1',
+    start: '2022-01-01T10:00:00',
+    end: '2022-01-01T12:00:00',
+  },
+  {
+    id: 2,
+    title: 'event 2',
+    start: '2021-12-29T13:00:00',
+    end: '2021-12-30T18:00:00',
+  },
+  { id: 3, title: 'event 3', start: '2021-06-17', end: '2021-06-20' },
+];
+
+function FullCalendarApp() {
+  return (
+    <div className="App">
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          center: 'dayGridMonth,timeGridWeek,timeGridDay new',
+        }}
+        customButtons={{
+          new: {
+            text: 'new',
+            click: () => console.log('new event'),
+          },
+        }}
+        events={events}
+        eventColor="red"
+        nowIndicator
+        dateClick={(e) => console.log(e.dateStr)}
+        eventClick={(e) => console.log(e.event.id)}
+      />
+    </div>
+  );
+}
+
+export default FullCalendarApp;
