9d6d318a7498ca70859d39e28d6e4a79859e8755
Damian Kaminski <arthuradd@yahoo.com>
2022-01-03 12:30:35 +0100

    Implemented adding event function

--- src/components/AccountSettings.js
+++ src/components/AccountSettings.js
@@ -1,21 +1,31 @@
-import {Link} from 'react-router-dom';
 import CalendarHeader from './CalendarHeader';
 
 function AccountSettings() {
-    return (
-      <>
+  return (
+      <div className="App">
         <CalendarHeader page="/Calendar"/>
-        <main>
-          <h2>Who are we?</h2>
-          <p>
-            That feels like an existential question, don't you
-            think?
-          </p>
-        </main>
-        <nav>
-          <Link to="/">Home</Link>
-        </nav>
-      </>
+        <div className="SettingSection" style={{ display: `flex`}}>
+          <div className="SettingsColumn" style={{
+            width: `100%`,
+            height: `100%`,
+            alignItems: `center`,
+            paddingLeft: `200px`,
+            paddingRight: `200px`,
+            display: `flex`,
+            flexDirection: `column`
+          }}>
+            <h1>Ustawienia</h1>
+            <form>
+              <label for="userName" style={{paddingRight: `5px`}}>Nazwa uzytkownika:</label>
+              <input type="text" id="userName" name="userName"></input><br></br><br></br>
+              <label for="password" style={{paddingRight: `5px`}}>Haslo:</label>
+              <input type="password" id="password" name="password"></input>
+            </form>
+            <button style={{position: `relative`, top: `10px`}}>Zapisz</button>
+
+          </div>
+        </div>  
+      </div>
     );
   }
 
--- src/components/FullCalendarApp.js
+++ src/components/FullCalendarApp.js
@@ -3,24 +3,30 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import CalendarHeader from './CalendarHeader';
+import React, { useState } from "react";
 
-const events = [
-  {
-    id: 1,
-    title: 'event 1',
-    start: '2022-01-01T10:00:00',
-    end: '2022-01-01T12:00:00',
-  },
-  {
-    id: 2,
-    title: 'event 2',
-    start: '2021-12-29T13:00:00',
-    end: '2021-12-30T18:00:00',
-  },
-  { id: 3, title: 'event 3', start: '2021-06-17', end: '2021-06-20' },
-];
+
+// const events = [
+//   {
+//     id: 1,
+//     title: 'event 1',
+//     start: '2022-01-01T10:00:00',
+//     end: '2022-01-01T12:00:00',
+//   },
+//   {
+//     id: 2,
+//     title: 'event 2',
+//     start: '2021-12-29T13:00:00',
+//     end: '2021-12-30T18:00:00',
+//   },
+//   { id: 3, title: 'event 3', start: '2021-06-17', end: '2021-06-20' },
+// ];
+
 
 function FullCalendarApp() {
+
+  const [events, setEvents] = useState([]);
+
   return (
     <div className="App">
       <div id="calHeader" className="basicHeader">
@@ -34,16 +40,31 @@ function FullCalendarApp() {
             center: 'dayGridMonth,timeGridWeek,timeGridDay new',
           }}
           customButtons={{
-            new: {
-              text: 'new',
-              click: () => console.log('new event'),
-            },
-          }}
+              new: {
+                text: 'new',
+                click: () => {
+                  var sdateStr = prompt('Enter a start date in YYYY-MM-DD format');
+                  var edateStr = prompt('Enter a end date in YYYY-MM-DD format');
+                  var titleStr = prompt('Enter a title of your event');
+                  var sdate = new Date(sdateStr + 'T00:00:00');
+                  var edate = new Date(edateStr + 'T00:00:00')
+
+                  if (!isNaN(sdate.valueOf()) && !isNaN(edate.valueOf())) {
+
+                    setEvents(events.concat({title: titleStr, start: sdate, end: edate}))
+                  }
+                },
+              },
+            }
+          }
           events={events}
           eventColor="red"
           nowIndicator
           dateClick={(e) => console.log(e.dateStr)}
           eventClick={(e) => console.log(e.event.id)}
+          editable={true}
+          selectable={true}
+          dayMaxEvents={true}
         />
       </div>
     </div>
