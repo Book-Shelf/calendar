f30088f9465290568204216651d0e58c94ec05e2
amajeczyk <amajerczyk414@gmail.com>
2022-01-15 17:45:17 +0100

    Added registration api
    Created connection between server and database.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose')
+const User = require('../model/userModel');
+const bcrypt = require('bcryptjs');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const mongodbURL = 'mongodb://localhost:27017/register'; //you have to install mongoDB and host on localhost
+mongoose.connect(mongodbURL,{useNewUrlParser: true, useUnifiedTopology: true});
+
+router.post('/register', async function(req, res) {  
+  const {username, email, textPassword} = req.body;
+  
+  if(!username || !email || !textPassword){
+    return res.json({status : 'error', error : "invalid data"});
+  }
+  
+  const password = await bcrypt.hash(textPassword, 10); //hashing password and save it to database
+
+  try{
+    const response = await User.create({
+      username,
+      email,
+      password
+    });
+    console.log('inserted user => ', response);
+  }catch(err){
+    if(err.code == 11000){
+      return res.json({status : 'error', error : Object.keys(err.keyPattern).pop() + " already in use"});
+    }else{
+      throw err;
+    }
+    
+  }
+  res.json({status : 'ok'});
+})
 
 module.exports = router;
