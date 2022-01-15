ba775af868507cbaf7ee38d86aeca348a298c6c2
amajeczyk <amajerczyk414@gmail.com>
2022-01-15 17:50:02 +0100

    Added submit functions to Registration component

--- src/components/login/Registration.jsx
+++ src/components/login/Registration.jsx
@@ -1,45 +1,85 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import CalendarHeader from '../CalendarHeader';
-import "./style.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import CalendarHeader from "../CalendarHeader";
+import "./style.css";
 
-class Registration extends React.Component{
-    /*constructor(props){
-        super(props);
-    }*/
+class Registration extends React.Component {
+  state = {
+    username: "",
+    email: "",
+    textPassword: "",
+  };
 
-    render() {
-            return (
-            <div>
-                <CalendarHeader page="/" />
-                <div className='content-wrapper'>
-                    <div className='header'>Registration</div>
-                    <div className='content'>
-                        <div className='form'>
-                            <div className='form-group'>
-                                <label htmlFor='username'>Username:</label>
-                                <input type='text' name='username' placeholder='username'/>
-                            </div>
-                            <div className='form-group'>
-                                <label htmlFor='email'>E-mail:</label>
-                                <input type='email' name='email' placeholder='email'/>
-                            </div> 
-                            <div className='form-group'>
-                                <label htmlFor='password'>Password:</label>
-                                <input type='text' name='password' placeholder='password'/>
-                            </div>      
-                        </div>    
-                    </div> 
-                    <div className="footer">
-                        <Link to="/Calendar">
-                            <button type='button' className="button">
-                                Sign up
-                            </button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-            )}
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const result = await fetch("http://localhost:3001/users/register", {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+    }).then((res) => res.json());
+
+    //handle login action
+    if (result.status === "ok") {
+      //successful registration
+      console.log("sucess");
+    } else {
+      console.log("fail", result.error);
+    }
+  };
+
+  //setting state with user input
+  handleChange = (event) => {
+    const atrributeName = event.target.name;
+    this.setState({ [atrributeName]: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <CalendarHeader page="/" />
+        <div className="content-wrapper">
+          <div className="header">Registration</div>
+          <div className="content">
+            <form className="form" onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="username">Username:</label>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="username"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">E-mail:</label>
+                <input
+                  type="email" /*email*/
+                  name="email"
+                  placeholder="email"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="text"
+                  name="textPassword"
+                  placeholder="password"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <button type="submit" className="button">
+                Sign up
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Registration
+export default Registration;
