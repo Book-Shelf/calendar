8e5d8ea31b9c2ecbeeb7cf87606cb1bfc19ed44e
rekin73 <runefor99@gmail.com>
2022-01-17 13:58:17 +0100

    Filtrowanie jakoś działa

--- src/components/group_list/GroupList.js
+++ src/components/group_list/GroupList.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import GroupPlaque from './GroupPlaque'
 
-export default function GroupList( props ) {
+export default function GroupList(props) {
+    const [checked, setChecked] = React.useState(Array.from(props.groupInstances));
+
+    const handleChange = (index) => {
+        console.log(checked[index])
+        let nc=Array.from(checked)
+        nc[index].checked=!nc[index].checked 
+        console.log(nc[index])
+        setChecked(nc);
+        props.toParentHandler(nc)
+    };
+
     return (
         <div style={{overflow: 'auto', height: `Calc(100vh - 48px)`, width: 'auto', float: 'left', backgroundColor: '#C71F28'}}>
-            {props.groupInstances.map((groupInstance, index) => {
-                return <GroupPlaque key={index} groupInstance={groupInstance} />
+            {checked.map((groupInstance, index) => {
+                return <GroupPlaque key={index} groupInstance={groupInstance} onChange={() => handleChange(index)} checked={checked[index]} />
             })}
         </div>
     )
